5787a5aee7fc5e852f316000e892542ad0090bbc
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-10-03 19:48:03 +0100

    Aggiunto bootstrap, axios, strutura ed scss

---
+++ src/components/Album.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+    export default{
+        name:"Album"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
---
+++ src/components/Albums.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    <Album/>
+  </div>
+</template>
+
+<script>
+    import Album from "./Album.vue"
+    export default{
+        name:"Albums",
+        components:{
+            Album
+        }
+    }
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
